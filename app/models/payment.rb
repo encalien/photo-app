c2f9ab6985e4ebcc72dcfaf43e67b157847e0d98
encalien <katarina.mursec@gmail.com>
2019-01-25 13:07:49 +0100

    fixed stripe issue, api key storage and sign up action

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -14,7 +14,7 @@ class Payment < ApplicationRecord
   end
 
   def process_payment
-    customer = Stripe::Customer.create email: email, card: token
+    customer = Stripe::Customer.create(email: email, source: token)
 
     Stripe::Charge.create customer: customer.id, 
                           amount: 1000,
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,15 +54,7 @@ Rails.application.configure do
 
   config.read_encrypted_secrets = true
 
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
